28e34d0487ae29397fa876760431a71f2fe030d4
arhm098 <myhammad.arham.0987711@gmail.com>
2022-09-21 00:13:16 +0500

    test student

--- venv/app.py
+++ venv/app.py
@@ -9,13 +9,15 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         if request.form.get("F_student") == 'student':
-            return render_template('student.html',MCQ_HEADING = "This is the question that is being asked!",option_1="option 1",option_2="option 2",option_3="option 3",option_4="option 4")
+            return render_template('student.html',MCQ_HEADING = "This is the question that is being asked!",option_1="option 1",option_2="option 2",option_3="option 3",option_4="option 4",right_option='RIGHT ANSWER')
         if request.form.get("F_teacher") == 'teacher':
             return render_template('teacher.html')
     return render_template("index.html");   
 
 @app.route("/student",methods = ['GET','POST'])
 def student():
+    if request.method == 'POST':
+        return render_template("ans.html")
     return render_template("student.html")
 
 @app.route("/teacher",methods = ['GET','POST'])
