5f5b7148b74237939b0aa0526515813b4162521f
adeelakmal <adeelakmal0@gmail.com>
2022-09-22 02:23:36 +0500

    nutsack flask bitch
    idk man just did somethig

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,6 @@
-
 from http.client import REQUEST_ENTITY_TOO_LARGE
 from pydoc import render_doc
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 db = sqlite3.connect('../test.db')
@@ -21,9 +20,6 @@ for row in cursor:
 
 app = Flask(__name__)
 
-@app.context_processor
-def context_processor(picked):
-    return dict(option=picked)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -32,32 +28,35 @@ def index():
         if request.form.get("F_student") == 'student':
             print(picked)
             print(MCQs[picked])
-            return render_template('student.html',clicked = '0',MCQs = MCQs)
+            return render_template('student.html', clicked='0', MCQs=MCQs)
         if request.form.get("F_teacher") == 'teacher':
-            return render_template('teacher.html',MCQs=MCQs)
+            return render_template('teacher.html', MCQs=MCQs)
     return render_template("index.html")
 
 
 @app.route("/student", methods=['GET', 'POST'])
 def student():
+    picked = open('./db/secretspecial.txt', 'r')
     if request.method == 'POST':
         answered = request.form.get("F_answer")
-        map = {'1':MCQs[picked][2],'2':MCQs[picked][3],'3':MCQs[picked][4],'4':MCQs[picked][5]}
+        map = {'1': MCQs[picked][2], '2': MCQs[picked][3],
+               '3': MCQs[picked][4], '4': MCQs[picked][5]}
         answered_c = map[answered]
         correct = 0
         if answered_c == answer:
             correct = 1
-        return render_template("student.html",clicked = '1',MCQs = MCQs,correct = correct,answer = answered_c)
+        return render_template("student.html", clicked='1', MCQs=MCQs, correct=correct, answer=answered_c)
     return render_template("student.html")
 
 
 @app.route("/teacher", methods=['GET', 'POST'])
 def teacher():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         picked = request.form.get('F_choice')
-        context_processor(picked)
-        return render_template("index.html")
-    return render_template("teacher.html",MCQs=MCQs)
+        file = open('./db/secretspecial.txt', 'w')
+        file.write(picked)
+        return redirect('/')
+    return render_template("teacher.html", MCQs=MCQs)
 
 
 if __name__ == "__main__":
