5008868866036c8ceb06a3c38652908818391e78
arhm098 <myhammad.arham.0987711@gmail.com>
2022-09-21 22:24:46 +0500

    MCQ part now just top G

--- venv/app.py
+++ venv/app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         if request.form.get("F_student") == 'student':
-            return render_template('student.html', MCQ_HEADING=question, option_1=option1, option_2=option2, option_3=option3, option_4=option4, right_option=answer)
+            return render_template('student.html',clicked = '0', MCQ_HEADING=question, option_1=option1, option_2=option2, option_3=option3, option_4=option4, right_option=answer)
         if request.form.get("F_teacher") == 'teacher':
             return render_template('teacher.html')
     return render_template("index.html")
@@ -39,7 +39,7 @@ def student():
         correct = 0
         if answered_c == answer:
             correct = 1
-        return render_template("answer.html", answer=answered_c, right_answer=answer,correct=correct)
+        return render_template("student.html",clicked = '1',MCQ_HEADING=question, option_1=option1, option_2=option2, option_3=option3, option_4=option4, right_option=answer, answer=answered_c, right_answer=answer,correct=correct)
     return render_template("student.html")
 
 
