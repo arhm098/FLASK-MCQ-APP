5039da40174e850a30e0d81a1a011465a1436fbb
arhm098 <myhammad.arham.0987711@gmail.com>
2022-12-23 16:04:05 +0500

    backend file added

---
+++ mcq-app/frontend/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+function App() {
+  let MCQprompt = "This is the question of the MCQ"
+  let choices = ["first choice","second choice","third choice","fourth choice"]
+  return (
+    <div>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <Typography component="h1" variant="h5">
+        {MCQprompt}
+      </Typography>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="option1" control={<Radio />} label={choices[0]} />
+        <FormControlLabel value="option2" control={<Radio />} label={choices[1]} />
+        <FormControlLabel value="option3" control={<Radio />} label={choices[2]} />
+        <FormControlLabel value="option4" control={<Radio />} label={choices[3]} />
+        <Button> Sjdsj</Button>
+      </RadioGroup>
+    </div>
+  );
+}
+
+export default App;
