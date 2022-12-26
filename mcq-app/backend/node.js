04dac0889c68a64f81841e816d6d000e76bd2db6
arhm098 <myhammad.arham.0987711@gmail.com>
2022-12-26 20:04:32 +0500

    .

--- mcq-app/backend/node.js
+++ mcq-app/backend/node.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+let MCQ = ["prompt of the MCQ","choice 1","choice 2","choice 3","choice 4","1"]
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// parse application/json
+app.use(bodyParser.json());
 app.use(express.static('public'));
+const cors = require('cors');
 
+app.use(cors());
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send({
+      MCQ
+  });
 });
-
+app.post('/', (req, res) => {
+  const data = req.body;
+  console.log(data);
+});
 app.listen(3001, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port 3001');
 });
